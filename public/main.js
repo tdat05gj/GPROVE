25645bb0ac45e94b92662bd3ac3df91dde614180
unknown <datdaty8@gmail.com>
2025-07-26 13:49:09 +0700

    prove

--- public/main.js
+++ public/main.js
@@ -1,17 +1,15 @@
-require('dotenv').config();
-
+// ==== Firebase & Cloudinary Config
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCSNOFt0FkPZCOWV4O8CnDBHaBRxcKEZ1Y",
+  authDomain: "gprove-2ce02.firebaseapp.com",
+  projectId: "gprove-2ce02",
+  storageBucket: "gprove-2ce02.appspot.com",
+  messagingSenderId: "899724956812",
+  appId: "1:899724956812:web:6e62bdfaa4fb35e7d697a9",
+  measurementId: "G-G8RLCMQHQ9"
 };
-
-const CLOUDINARY_UPLOAD_URL = process.env.CLOUDINARY_UPLOAD_URL;
-const CLOUDINARY_UPLOAD_PRESET = process.env.CLOUDINARY_UPLOAD_PRESET;
+const CLOUDINARY_UPLOAD_URL = "https://api.cloudinary.com/v1_1/gprove/image/upload";
+const CLOUDINARY_UPLOAD_PRESET = "gprove_unsigned";
 
 const TEAM_COLORS = {
   pink:   {main: "#FF54D7", name: "Pink"},
